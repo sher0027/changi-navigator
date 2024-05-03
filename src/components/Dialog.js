84d4d461256d2676e001d2e2588f260407f5738f
wongsyi <wongshangyi99@gmail.com>
2024-05-03 20:26:11 +0800

    GPT able to get current location
    Integrate GPT Api and Maps Reverse Geocoding API

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Box, Typography, TextField, IconButton} from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
-import { OpenAI } from 'openai';
+import {OpenAI} from 'openai';
 import Loading from './Loading';
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
@@ -11,18 +11,20 @@ const API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
 const ASSISTANT_ID = process.env.NEXT_PUBLIC_ASSISTANT_ID;
 const ASSISTANT_NAME = process.env.NEXT_PUBLIC_ASSISTANT_NAME;
 const openai = new OpenAI({apiKey: API_KEY, dangerouslyAllowBrowser: true});
-
+const MAP_KEY = process.env.NEXT_PUBLIC_MAP_KEY;
 
 function Dialog() {
     const [threadId, setThreadId] = useState('');
     const [lastRunId, setLastRunId] = useState('');
     const [input, setInput] = useState('');
     const [messages, setMessages] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [currentLocation, setCurrentLocation] = useState(null);
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Step 2: Initialise a new thread for each user instance
     useEffect(() => {
+        // Initialise a GPT session
         async function initThread() {
             try {
                 const thread = await openai.beta.threads.create();
@@ -34,6 +36,7 @@ function Dialog() {
         }
 
         initThread();
+        getCurrentLocation()
     }, []);
 
 
@@ -65,33 +68,62 @@ function Dialog() {
         setIsLoading(false);
     }
 
+    //Wait the server to finish generating or finish the input required
     async function waitRunToComplete(thread_id, run_id) {
         let run = await openai.beta.threads.runs.retrieve(thread_id, run_id);
         let new_run_status = run.status;
 
         //Polling
-        while (new_run_status === 'queued' || new_run_status === 'in_progress') {
+        while (new_run_status === 'queued' || new_run_status === 'in_progress' ||new_run_status === "requires_action") {
             run = await openai.beta.threads.runs.retrieve(thread_id, run_id);
             new_run_status = run.status;
             if (new_run_status === "requires_action" && run.required_action.type === "submit_tool_outputs") {
                 run = await openai.beta.threads.runs.retrieve(thread_id, run_id);
-                let calendar_event = JSON.parse(run.required_action.submit_tool_outputs.tool_calls[0].function.arguments)
+                let function_name = run.required_action.submit_tool_outputs.tool_calls[0].function.name
                 let tool_call_id = run.required_action.submit_tool_outputs.tool_calls[0].id
-                await addEvent(calendar_event)
-                let tool_output = await openai.beta.threads.runs.submitToolOutputs(
-                    thread_id,
-                    run_id,
-                    {
-                        tool_outputs: [
-                            {
-                                tool_call_id: tool_call_id,
-                                output: "Done inserting",
-                            },
-                        ],
+                if (function_name === 'insert_event_to_google_calendar') {
+                    let calendar_event = JSON.parse(run.required_action.submit_tool_outputs.tool_calls[0].function.arguments)
+                    await addEvent(calendar_event)
+                    let tool_output = await openai.beta.threads.runs.submitToolOutputs(
+                        thread_id,
+                        run_id,
+                        {
+                            tool_outputs: [
+                                {
+                                    tool_call_id: tool_call_id,
+                                    output: "Done inserting",
+                                },
+                            ],
+                        }
+                    );
+                } else if (function_name === 'get_current_location'){
+                    await getCurrentLocation()
+                    let currentLocationDescription
+                    console.log("current long lat: ", currentLocation)
+                    if (currentLocation !== null) {
+                        currentLocationDescription = await fetchLocationDescription(currentLocation.lat, currentLocation.lng)
                     }
-                );
-            }
+                    else{
+                        currentLocationDescription = 'Location not available, request user to input manually!'
+                    }
 
+                    console.log("location description:", currentLocationDescription)
+
+                    let tool_output = await openai.beta.threads.runs.submitToolOutputs(
+                        thread_id,
+                        run_id,
+                        {
+                            tool_outputs: [
+                                {
+                                    tool_call_id: tool_call_id,
+                                    output: currentLocationDescription,
+                                },
+                            ],
+                        }
+                    );
+                }
+
+            }
             await new Promise(resolve => setTimeout(resolve, 500));
         }
     }
@@ -105,6 +137,43 @@ function Dialog() {
             newMessageList = await openai.beta.threads.messages.list(thread_id);
         }
         return newMessageList.data[0].content[0].text.value;
+    }
+
+    async function getCurrentLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const {latitude, longitude} = position.coords;
+                    setCurrentLocation({lat: latitude, lng: longitude});
+                },
+                (error) => {
+                    console.error(error);
+                }
+            );
+        } else {
+            console.error("Geolocation is not supported by this browser.");
+        }
+    }
+
+    async function fetchLocationDescription(lat, lng) {
+        const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&location_type=ROOFTOP&key=${MAP_KEY}`;
+
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log("data", data);
+            if (data.status === 'OK'){
+                return data.results[0].formatted_address
+            }
+            else{
+                return 'Location not available, request user to input manually!'
+            }
+        } catch (error) {
+            console.error('Error fetching geocode data:', error);
+        }
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -158,60 +227,60 @@ function Dialog() {
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     return (
-            <Box sx={{ margin: 'auto', position: 'relative'}}>
-                <Box
-                    sx={{
-                        bgcolor: '#F0F2F6',
-                        margin: 'auto',
-                        borderRadius: '20px',
-                        padding: '20px 20px 65px',
-                        maxHeight: '350px',
-                        width: '600px',
-                        overflowY: 'auto',
-                        '::-webkit-scrollbar': {
-                            display: 'none'
-                        },
-                        scrollbarWidth: 'none'
-                    }}>
-                    {messages.map((msg, index) => (
-                        <div key={index} style={{marginBottom: '20px'}}>
-                            <ReactMarkdown children={msg.name === 'You' ? 'You:' : 'Assistant:'}
-                                           remarkPlugins={[remarkGfm]}/>
-                            <ReactMarkdown children={msg.text} remarkPlugins={[remarkGfm]}/>
-                            <br/>
-                        </div>
+        <Box sx={{margin: 'auto', position: 'relative'}}>
+            <Box
+                sx={{
+                    bgcolor: '#F0F2F6',
+                    margin: 'auto',
+                    borderRadius: '20px',
+                    padding: '20px 20px 65px',
+                    maxHeight: '350px',
+                    width: '600px',
+                    overflowY: 'auto',
+                    '::-webkit-scrollbar': {
+                        display: 'none'
+                    },
+                    scrollbarWidth: 'none'
+                }}>
+                {messages.map((msg, index) => (
+                    <div key={index} style={{marginBottom: '20px'}}>
+                        <ReactMarkdown children={msg.name === 'You' ? 'You:' : 'Assistant:'}
+                                       remarkPlugins={[remarkGfm]}/>
+                        <ReactMarkdown children={msg.text} remarkPlugins={[remarkGfm]}/>
+                        <br/>
+                    </div>
 
-                    ))}
-                    <Loading open={isLoading}/>
-                    <Box sx={{
-                        position: 'absolute',
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        bgcolor: '#F0F2F6',
-                        padding: '10px',
-                        borderBottomLeftRadius: '20px',
-                        borderBottomRightRadius: '20px',
-                        zIndex: 1000
-                    }}>
-                        <Box sx={{display: 'flex'}}>
-                            <TextField
-                                fullWidth
-                                id="outlined-basic"
-                                label="Type a message"
-                                variant="outlined"
-                                size="small"
-                                value={input}
-                                onChange={(e) => setInput(e.target.value)}
-                                onKeyDown={handleKeyDown}
-                            />
-                            <IconButton color="primary" aria-label="send" onClick={handleUserInput}>
-                                <SendIcon/>
-                            </IconButton>
-                        </Box>
+                ))}
+                <Loading open={isLoading}/>
+                <Box sx={{
+                    position: 'absolute',
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    bgcolor: '#F0F2F6',
+                    padding: '10px',
+                    borderBottomLeftRadius: '20px',
+                    borderBottomRightRadius: '20px',
+                    zIndex: 1000
+                }}>
+                    <Box sx={{display: 'flex'}}>
+                        <TextField
+                            fullWidth
+                            id="outlined-basic"
+                            label="Type a message"
+                            variant="outlined"
+                            size="small"
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
+                            onKeyDown={handleKeyDown}
+                        />
+                        <IconButton color="primary" aria-label="send" onClick={handleUserInput}>
+                            <SendIcon/>
+                        </IconButton>
                     </Box>
                 </Box>
             </Box>
+        </Box>
     );
 }
 
